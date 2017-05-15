21ec6588af8b92616c387b8bb95f0f01cfdc7176
Mathieu Girard <mat.girard@gmail.com>
2017-05-15 18:14:00 +0000

    dev

--- matalarm.js
+++ matalarm.js
@@ -1,26 +1,34 @@
 const ALARM_PORT = "/dev/ttyUSB0"
 const API_PORT = 4000
+const ALARM_PIN = ***REMOVED***
 
 var express = require('express');
-
 var app = express();
+app.listen(API_PORT);
+
+var Alarm = require('ad2usb');
+
+var alarm = Alarm.connect('127.0.0.1', 10001, function() {
+
+	app.get('/disarm', function(req, res){
+	      alarm.disarm(ALARM_PIN);
+	      res.sendStatus(200);
+	});
 
 
-app.get('/disarm', function(req, res){
-      res.send('disarm');
+	app.get('/arm/full', function(req, res){
+	      alarm.armAway(ALARM_PIN);
+	      res.sendStatus(200);
+	});
+
+
+	app.get('/arm/nuit', function(req, res){
+	      alarm.armStay(ALARM_PIN);
+	      res.sendStatus(200);
+	});
+
+	app.get('/status', function(req, res){
+	      res.send(alarm.isArmed());
+	});
+
 });
-
-
-app.get('/arm/full', function(req, res){
-      res.send('armfull');
-});
-
-
-app.get('/arm/nuit', function(req, res){
-      res.send('armnuit');
-});
-
-app.get('/status', function(req, res){
-      res.send('status');
-});
-
